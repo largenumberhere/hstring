38a6bcf206e37ccb2d3a31ae6cdae325b75f4d9b
largenumberhere <60594766+largenumberhere@users.noreply.github.com>
2023-09-17 13:24:51 +0000

    Sun, Sep 17, 2023, 11:24 PM +10:00

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,14 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests{
+    use crate::hstring_new;
+
+    #[test]
+    fn creation(){
+        let hstring  = unsafe{ hstring_new() };
+        println!("{:p}",hstring.contents);
+    }
+}
